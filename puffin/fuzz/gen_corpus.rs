bb3d8e07ba217a3ea97755aef9996814f81e499b
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-16 00:03:27 -0400

    More corpus

--- puffin/fuzz/gen_corpus.rs
+++ puffin/fuzz/gen_corpus.rs
@@ -37,7 +37,34 @@ fn main() {
         stream
     };
 
+    let stream_3 = {
+        let mut stream = Stream::default();
+        let t0 = stream.begin_scope(100, "top", "top.rs", "data_top");
+        let m1 = stream.begin_scope(200, "middle_earth_0", "middle.rs", "data_middle_0");
+        stream.end_scope(m1, 500);
+        let m1 = stream.begin_scope(300, "middle_earth_1", "middle.rs:42", "data_middle_1");
+        stream.end_scope(m1, 400);
+        stream.end_scope(t0, 700);
+        stream
+    };
+
     // Write the streams to a file.
     std::fs::write("stream_1.bin", stream_1.bytes()).unwrap();
     std::fs::write("stream_2.bin", stream_2.bytes()).unwrap();
+    std::fs::write("stream_3.bin", stream_3.bytes()).unwrap();
+
+    // Read the streams from the file.
+    let stream_1 = Stream::from(std::fs::read("stream_1.bin").unwrap());
+    let stream_2 = Stream::from(std::fs::read("stream_2.bin").unwrap());
+    let stream_3 = Stream::from(std::fs::read("stream_3.bin").unwrap());
+
+    // Print the length of the streams.
+    println!("stream_1.len(): {}", stream_1.len());
+    println!("stream_2.len(): {}", stream_2.len());
+    println!("stream_3.len(): {}", stream_3.len());
+
+    // Parse the streams.
+    let _ = puffin::StreamInfo::parse(stream_1).unwrap();
+    let _ = puffin::StreamInfo::parse(stream_2).unwrap();
+    let _ = puffin::StreamInfo::parse(stream_3).unwrap();
 }
